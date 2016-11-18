502a30ac1995225f942bfdff4ca50cffd9e3bfdb
Jacco van den Hoven <jacco.vanden.hoven@capgemini.com>
2016-11-18 22:52:14 +0100

    added basic post request

--- appinion/www/js/controllers.js
+++ appinion/www/js/controllers.js
@@ -1,10 +1,20 @@
 angular.module('app.controllers', [])
-  
-.controller('aPPINIONCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+
+.controller('aPPINIONCtrl', ['$scope', '$http', '$stateParams', '$ionicLoading', 'BlankFactory',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $http, $stateParams, $ionicLoading, BlankFactory) {
+  var vm = this;
 
+  vm.searchOpinions = function(input) {
+    console.log('init search');
 
-}])
- 
+    console.log($scope.input);
+  };
+
+  $scope.submit = function(){
+    console.log('launching request...');
+    BlankFactory.postFunction($scope.input);
+  }
+
+}]);
--- appinion/www/js/services.js
+++ appinion/www/js/services.js
@@ -1,6 +1,24 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('BlankFactory', ['$http', '$q', '$ionicLoading', function($http, $q, $ionicLoading){
+
+  function postFunction(input) {
+    var link = 'http://demo8089816.mockable.io/search';
+    $ionicLoading.show({
+      duration: 3000,
+      noBackdrop: true,
+      template: 'Loading...'
+    });
+
+    $http.post(link, {searchInput: input}).then(function (res) {
+      $ionicLoading.hide();
+      console.log('post succeeded');
+    });
+  }
+
+  return {
+    postFunction: postFunction
+  };
 
 }])
 
