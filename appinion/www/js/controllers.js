3a485a6256a0910e8805f73252ee6f4581246f04
Jacco van den Hoven <jacco.vanden.hoven@capgemini.com>
2016-11-19 02:22:59 +0100

    Add really awesome input field

--- appinion/www/js/controllers.js
+++ appinion/www/js/controllers.js
@@ -3,15 +3,47 @@ angular.module('app.controllers', [])
 .controller('aPPINIONCtrl', ['$scope', '$stateParams', '$state', 'BlankFactory',
     function($scope, $stateParams, $state, BlankFactory) {
 
+      var input = [];
+      var tagCollection = [];
+
+
+      $(function(){ // DOM ready
+
+        // ::: TAGS BOX
+        $("#tags input").on({
+          focusout : function() {
+            var txt = this.value.replace(/[^a-z0-9\+\-\.\#]/ig,''); // allowed characters
+
+            if(txt) $("<span/>",{text:txt.toLowerCase(), insertBefore:this});
+            this.value="";
+          },
+          keyup : function(ev) {
+            // if: comma|enter (delimit more keyCodes with | pipe)
+
+            if(/(188|13|32)/.test(ev.which)) {
+              tagCollection.push(this.value);
+              console.log('collection - ' + tagCollection);
+              console.log('first element - ' + tagCollection[0]);
+              $(this).focusout();
+            }
+            if((/8/).test(ev.which)) $(this).remove();
+          }
+        });
+        $('#tags').on('click', 'span', function() {
+          if(confirm("Remove "+ $(this).text() +"?")) $(this).remove();
+        });
+
+      });
+
         $scope.chartProvider = false;
-        $scope.input = '';
         $scope.waarde = 90;
-        
+
         var connectionURL = 'http://demo8089816.mockable.io/search';
 
         $scope.submit = function() {
-            console.log('launching request...');
-
+            console.log('initial input = ' + $scope.input);
+            input = $scope.input.split(" ");
+            console.log('launching request with tags:' + input);
             BlankFactory.postFunction(connectionURL, $scope.input)
                 .then(function(response) {
                     $scope.chartProvider = true;
